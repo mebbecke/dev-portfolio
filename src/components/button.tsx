7e692c3d32d39532fe6d6becd18b4093b2f30387
Marina Ebbecke <m.ebbecke@gmail.com>
2024-09-03 10:13:08 -0300

    fix: :bug: applies correction to Resume download button and to button styles

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -8,9 +8,9 @@ const buttonVariants = tv({
       primary:
         "text-white dark:border dark:border-solid dark:border-sky border-none bg-sky dark:bg-transparent dark:hover:bg-sky hover:bg-darkGray",
       secondary:
-        "dark:text-gray-300 dark:text-sky text-darkGray border-darkGray border border-solid dark:border-gray-600",
+        "dark:text-gray-300 dark:text-sky text-darkGray border-darkGray border border-solid dark:border-gray-600 dark:hover:bg-darkGray hover:bg-gray-300",
       ghost:
-        "dark:text-white border-none rounded-full hover:text-darkGray dark:hover:bg-darkGray",
+        "dark:text-white border-none hover:text-darkGray dark:hover:text-gray-300",
       social:
         "dark:text-gray-400 text-darkGray border dark:border-gray-600 border-darkGray p-2 dark:hover:text-lightGray dark:hover:border-lightGray hover:text-darkPurple hover:border-darkPurple",
     },
