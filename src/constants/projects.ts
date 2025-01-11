4136a85a7438c703535521743607a18e97edadc3
Marina Ebbecke <m.ebbecke@gmail.com>
2025-01-11 16:51:51 -0300

    feat: adds Carometro project and updates Resume file

--- src/constants/projects.ts
+++ src/constants/projects.ts
@@ -2,10 +2,19 @@ import { Project } from "../types/project"
 
 export const projects: Project[] = [
   {
-    title: "Portfólio Dev",
+    title: "Carômetro Escolar",
+    description:
+      "Aplicativo web para facilitar a identificação de alunos de uma sala de aula.",
+    image: "/images/carometro-image.png",
+    websiteUrl: "https://carometro-escolar.vercel.app/",
+    githubUrl: "https://github.com/mebbecke/carometro-escolar",
+    techs: ["React", "TypeScript", "TailwindCSS"],
+  },
+  {
+    title: "Meu Portfólio",
     description:
       "Portfólio pessoal desenvolvido para mostrar meus projetos e artigos sobre meu aprendizado em tecnologia.",
-    image: "/images/website-mockup.png",
+    image: "/images/my-portfolio-image.png",
     websiteUrl: "https://marinaebbecke.dev.br/",
     githubUrl: "https://github.com/mebbecke/dev-portfolio",
     techs: ["React", "TypeScript", "TailwindCSS"],
